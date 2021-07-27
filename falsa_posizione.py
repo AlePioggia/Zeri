ee5f91bb1cd5cd0c7e8d946630a9425bb8d03835
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-07-27 10:07:57 +0200

    modify regula_falsi

--- falsa_posizione.py
+++ falsa_posizione.py
@@ -1,28 +1,26 @@
 import numpy as np
 import math
 
-def sign(x) : return math.copysign(1, x) #Ritorna 1 se x > 0, -1 se x < 0, 0 se b è 0
+def sign(x):
+    return math.copysign(1, x)
 
-def check_tolerance(a, b, eps, tol):
-    return abs(b - a) >= tol + eps * max(abs(a), abs(b))
-
-def regula_falsi(f, a, b, tol, max_iterations):
-    eps = 2**52
-    if sign(f(a) == sign(f(b))) > 0:
-        exit()
-    n_iterations = 0
-    x = []
-    while n_iterations < max_iterations and check_tolerance(a, b, eps, tol) and abs(f(a)) >= tol:
-        alpha = a - f(a) * (b - a) / (f(b) - f(a))
-        x.append(alpha)
-        n_iterations = n_iterations + 1
-        if f(alpha) == 0:
-            break
-        elif sign(f(alpha) == sign(f(a))) > 0:
-            a = alpha 
-        elif sign(f(alpha)) == sign(f(b)) < 0:
-            b = alpha
-    return alpha, n_iterations, x
-        
+def bisezione(f, a, b, tol):
+    if sign(f(a)) == sign(f(b)):
+        return "You can't use this method on this interval"
+    max_iterations = math.ceil(math.log((b - a) / tol) / math.log(2))
+    list = []
+    i = 0
+    eps = np.spacing(1)
+    while i < max_iterations and \
+        abs(b - a) >= tol + eps*min(abs(a), abs(b)):
+        x = a + (b - a)*0.5
+        list.append(x)
+        if f(x) == 0:
+            return x, i + 1, list
+        if sign(f(a)) != sign(f(x)):
+            b = f(x)
+        if sign(f(x)) != sign(f(b)):
+            a = f(x)
+    return x, i, list
 
 
