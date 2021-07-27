3a42f0e6d171d9866ea6c7ffbe281f06ca49cca6
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-07-27 10:13:15 +0200

    correct corde.py

--- corde.py
+++ corde.py
@@ -1,12 +1,14 @@
 def corde(f,df, x0, tol_x,tol_f, max_iterations ): #df() = f'()
-     x = []
+     list = []
      m = df(x0)
-     n_iterations = 0
+     i = 0
      while True:
-        x1 = x0 - (f(x0) / m) # Ricavata 
-        n_iterations = n_iterations + 1
-        x.append(x1)
-        x0 = x1
-        if (n_iterations < max_iterations and abs(f(x1) >= tol_f and abs(f(x0 / m)) >= tol_x * abs(x1))) == False:
-            break
-     return x1, n_iterations, x
+        x = x0 - (f(x0) / m)
+        i = i + 1
+        list.append(x)
+        x0 = x
+        if (i < max_iterations and \
+            abs(f(x) >= tol_f and \
+                abs(f(x0) / m) >= tol_x * abs(x))) == False:
+                break
+     return x, i, list
