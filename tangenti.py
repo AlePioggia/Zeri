59b5ffeb16ba02026b5119b3e00270000af96e35
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-07-28 08:41:39 +0200

    correct statement

--- tangenti.py
+++ tangenti.py
@@ -13,8 +13,8 @@ def tangenti(f,df, x0, tol_x,tol_f, max_iterations ): #x_minus_1 and x0 = valori
         else:
             print("Derivata nulla in x0")
             break
-        if i < max_iterations and \
+        if (i < max_iterations and \
             abs(f(x)) >= tol_f and \
-                abs(f(x0) / m) >= tol_x*abs(x) == False:
+                abs(f(x0) / m) >= tol_x*abs(x)) == False:
                 break
      return x, i,list
