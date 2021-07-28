7e757f0a56f6fa1bc2bc7fbfb4e1a7ce3ab613b2
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-07-28 11:15:29 +0200

    fix methods

--- secanti.py
+++ secanti.py
@@ -3,14 +3,14 @@ def secanti(f,x_minus_1, x0, tol_x,tol_f, max_iterations ): #x_minus_1 and x0 = 
      list = []
      i = 0
      while True:
-        m = (f(x0) - f(x_minus_1)) / (x0 - x_minus_1) #coefficiente angolare secante
-        x1 = x0 - (f(x0) / m) # x0 - distanza fra x0 e x1 = x1
+        d = f(x0) * (x0 - x_minus_1) / (f(x0) - f(x_minus_1))
+        x1 = x0 - d
         list.append(x1)  
         x_minus_1 = x0
         x0 = x1
         i = i + 1
-        if ( i < max_iterations and \
-            abs(f(x1))>=tol_x and \
-                abs(f(x0)/m)>=(tol_x * abs(x1)) == False ):
+        if (i < max_iterations and \
+            abs(f(x1))>=tol_f and \
+                abs(d)>=(tol_x * abs(x1))) == False:
                 break
      return x1, i, list
--- tangenti.py
+++ tangenti.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 #per df si intende la derivata
+#Versione personale attualmente sbagliata 
 def tangenti(f,df, x0, tol_x,tol_f, max_iterations ): #x_minus_1 and x0 = valori d'innesco
      list = []
      m = df(x0)
@@ -18,3 +19,29 @@ def tangenti(f,df, x0, tol_x,tol_f, max_iterations ): #x_minus_1 and x0 = valori
                 abs(f(x0) / m) >= tol_x*abs(x)) == False:
                 break
      return x, i,list
+
+#versione della prof
+def newton(f,df, x0, tol_x,tol_f, max_iterations ):
+    list = []
+    eps = np.spacing(1)
+    i = 1
+    if abs(df(x0)) > eps:
+        x = x0 - f(x0)/df(x0)
+        list.append(x)
+    else:
+        print("Newton : Derivata nulla in x0")
+        return [], 0, []
+    while i < max_iterations and \
+        abs(f(x)) >= tol_f and \
+            abs(f(x0)/ df(x0)) >= tol_x*abs(x):
+            x0 = x
+            if abs(df(x0)) > eps:
+                x = x0 - f(x0)/df(x0)
+                list.append(x)
+                i = i + 1
+            else:
+                print("Newton : Derivata nulla in x0")
+                return [], 0, []
+            if i == max_iterations:
+                print("Newton : Raggiunto il massimo numero di iterazioni")
+    return x, i, list
