2cf50d38b473a42efa363479c87c7e6bd4cd8582
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-07-29 10:00:21 +0200

    add stima ordine

---
+++ stima_ordine.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def set_numerator(x_list, i, k):
+    return np.log(abs(x_list[k + 2] - x_list[k + 3]) / abs(x_list[k + 1] - x_list[k + 2]))
+def set_denominator(x_list, i, k):
+    return np.log(abs(x_list[k + 1] - x_list[k + 2]) / abs(x_list[k] - x_list[k + 1]))
+
+def stima_ordine(x_list,i):
+      p=[]
+      for k in range(i-3):
+          p = []
+          numerator = set_numerator(x_list, i, k)
+          denominator = set_denominator(x_list, i, k)
+          p.append(numerator/denominator)
+          ordine=p[-1]
+      return ordine
