3da04337958eb0de8a0f6db05bd07ef99c34ee3e
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-07-29 08:26:27 +0200

    correct falsa posizione

--- falsa_posizione.py
+++ falsa_posizione.py
@@ -4,24 +4,24 @@ import math
 def sign(x):
     return math.copysign(1, x)
 
-def bisezione(f, a, b, tol):
+def regula_falsi(f, a, b, tol, max_iterations):
     if sign(f(a)) == sign(f(b)):
-        return "You can't use this method on this interval"
-    max_iterations = math.ceil(math.log((b - a) / tol) / math.log(2))
-    list = []
+        return 0, 0, []
+    x_list = []
     i = 0
     eps = np.spacing(1)
+    x = f(a)
     while i < max_iterations and \
-        abs(b - a) >= tol + eps*min(abs(a), abs(b)):
-        x = a + (b - a)*0.5
-        list.append(x)
-        if f(x) == 0:
-            return x, i + 1, list
-        if sign(f(a)) != sign(f(x)):
-            b = f(x)
-        if sign(f(x)) != sign(f(b)):
-            a = f(x)
-	i = i + 1
-    return x, i, list
+        abs(f(x)) >= tol and \
+            abs(b - a) >= tol + eps*max(abs(a), abs(b)):
+            x = -f(a)*((b - a)/f(b) - f(a)) + a
+            x_list.append(x)
+            if f(x) == 0:
+                return 0, 0, []
+            if sign(f(a)) != sign(f(x)):
+                b = f(x)
+            if sign(f(x)) != sign(f(x)):
+                a = f(x)
+            i = i + 1
 
 
