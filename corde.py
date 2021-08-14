37113138c88b940d5b489168f40777b0054ea2ce
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-08-14 09:13:00 +0200

    fix corde method

--- corde.py
+++ corde.py
@@ -1,8 +1,8 @@
 def corde(f,df, x0, tol_x,tol_f, max_iterations ): #df() = f'()
-     list = []
-     m = df(x0)
-     i = 0
-     while True:
+    list = []
+    m = df(x0)
+    i = 0
+    while True:
         x = x0 - (f(x0) / m)
         i = i + 1
         list.append(x)
@@ -11,4 +11,4 @@ def corde(f,df, x0, tol_x,tol_f, max_iterations ): #df() = f'()
             abs(f(x) >= tol_f and \
                 abs(f(x0) / m) >= tol_x * abs(x))) == False:
                 break
-     return x, i, list
+    return x, i, list
