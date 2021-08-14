5a1dc47beee658813c2ab2473ebcdb6d1b277b09
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-08-14 09:11:13 +0200

    fix bisezione method

--- bisezione.py
+++ bisezione.py
@@ -1,26 +1,26 @@
 import numpy as np
 import math
 
-def sign(x):
-    return math.copysign(1, x)
+def sign(x): return math.copysign(1, x)
 
-def bisezione(f, a, b, tol):
+def bisez(f, a, b, tol):
     if sign(f(a)) == sign(f(b)):
-        return "You can't use this method on this interval"
-    max_iterations = math.ceil(math.log((b - a) / tol) / math.log(2))
-    list = []
+        print("You can't use this method on this interval")
+        return [], 0, []
+    max_iterations = int(math.ceil(math.log((b - a) / tol) / math.log(2)))
+    x_list = []
     i = 0
     eps = np.spacing(1)
     while i < max_iterations and \
-        abs(b - a) >= tol + eps*min(abs(a), abs(b)):
+        abs(b - a) >= tol + eps*max(abs(a), abs(b)):
         x = a + (b - a)*0.5
-        list.append(x)
+        x_list.append(x)
+        i = i + 1
         if f(x) == 0:
-            return x, i + 1, list
+            break
         if sign(f(a)) != sign(f(x)):
-            b = f(x)
+            b = x
         if sign(f(x)) != sign(f(b)):
-            a = f(x)
-    	i = i + 1
-    return x, i, list
+            a = x
+    return x, i, x_list
 
