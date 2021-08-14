3f947d0732f1e56ed33fbbaa25f88bff965ba23a
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-08-14 09:15:21 +0200

    fix iterazione method

--- iterazione.py
+++ iterazione.py
@@ -4,12 +4,13 @@ def iterazione(g, x0, tolx, max_iterations):
     x_list = []
     i = 0
     while True:
+        x_list.append(x0) if i == 0 else print("it", i)
         x = g(x0)
-        list.append(x)
+        x_list.append(x)
         d = x - x0
-        x0 = x
         i = i + 1
-        if i < max_iterations and \
-            abs(d) >= tolx*abs(x):
+        x0 = x
+        if not (i <= max_iterations and \
+            abs(d) >= tolx*abs(x)):
             break
     return x, i, x_list        
