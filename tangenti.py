67ab455fe124c3b16945559bb58d3818ff54d12d
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-08-14 09:14:20 +0200

    fix tangenti

--- tangenti.py
+++ tangenti.py
@@ -1,14 +1,15 @@
 import numpy as np
 
-def tangenti(f,df, x0, tol_x,tol_f, max_iterations ): 
-     list = []
-     i = 0
-     while True:
+def newton(f,df, x0, tol_x,tol_f, max_iterations ): #x_minus_1 and x0 = valori d'innesco
+    list = []
+    m = df(x0)
+    i = 0
+    while True:
         if (abs(df(x0)) > np.spacing(1)): #se e' inferiore ad eps e' nulla e si ferma
-              x = x0 - (f(x0) / df(x0))
-              list.append(x)
-              i = i + 1
-              x0 = x
+            x = x0 - (f(x0) / df(x0))
+            list.append(x)
+            i = i + 1
+            x0 = x
         else:
             print("Derivata nulla in x0")
             break
@@ -16,4 +17,4 @@ def tangenti(f,df, x0, tol_x,tol_f, max_iterations ):
             abs(f(x)) >= tol_f and \
                 abs(f(x0) / df(x0)) >= tol_x*abs(x)) == False:
                 break
-     return x, i,list
+    return x, i,list
