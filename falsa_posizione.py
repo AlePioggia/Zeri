67c7d308ab36fd884265df5f985188d5c712e7c1
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-08-14 09:12:19 +0200

    fix falsa posizione method

--- falsa_posizione.py
+++ falsa_posizione.py
@@ -6,22 +6,23 @@ def sign(x):
 
 def regula_falsi(f, a, b, tol, max_iterations):
     if sign(f(a)) == sign(f(b)):
-        return 0, 0, []
+        return [], 0, []
     x_list = []
     i = 0
     eps = np.spacing(1)
-    x = f(a)
+    x = a
     while i < max_iterations and \
         abs(f(x)) >= tol and \
             abs(b - a) >= tol + eps*max(abs(a), abs(b)):
-            x = -f(a)*((b - a)/f(b) - f(a)) + a
+            x = a -f(a)*(b-a)/(f(b)-f(a))
             x_list.append(x)
-            if f(x) == 0:
-                return 0, 0, []
-            if sign(f(a)) != sign(f(x)):
-                b = f(x)
-            if sign(f(x)) != sign(f(x)):
-                a = f(x)
             i = i + 1
+            if f(x) == 0:
+                break
+            if sign(f(a)) != sign(f(x)):
+                b = x
+            if sign(f(x)) != sign(f(b)):
+                a = x
+    return x, i, x_list
 
 
